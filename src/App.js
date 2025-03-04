cf5efbd26b58979edc085e15d5363af7351174a9
Soufiane El Barji <soufiane.elbarji2004@gmail.com>
2025-03-04 15:56:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ import InstagramIcon from './assets/icons/InstagramIcon';
 import LinkedInIcon from './assets/icons/LinkedInIcon';
 import AboutImageGallery from './AboutImageGallery';
 import { Analytics } from '@vercel/analytics/react';
-import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-B4T7ETSJT5");
-
-ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
-
-useEffect(() => {
-  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-}, []);
-
-const handleButtonClick = () => {
-  ReactGA.event({
-    category: 'User',
-    action: 'Clicked a button'
-  });
-};
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
